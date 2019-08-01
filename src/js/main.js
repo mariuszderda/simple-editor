1aeeb1596cdddb33ac56964496c4fa2596842ca1
Mariusz Derda <mariusz.derda@gmail.com>
2019-08-02 00:12:40 +0200

    feat: add js local storage code

--- src/js/main.js
+++ src/js/main.js
@@ -16,8 +16,18 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const message = document.querySelector('.form__message-js');
+const label = document.querySelector('.form__label--js');
+const buttonLoad = document.querySelector('.form__button--load-js');
+const buttonSave = document.querySelector('.form__button--save-js');
 
 
-console.log(`Hello world!🚀`);
+buttonSave.addEventListener('click', (e) => {
+  e.preventDefault();
+  localStorage.setItem('message', message.value);
+});
 
-
+buttonLoad.addEventListener('click', (e) => {
+  e.preventDefault();
+  message.value = localStorage.getItem('message');
+});
